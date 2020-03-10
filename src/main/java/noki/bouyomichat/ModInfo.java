76d8e7c57e629ea8dd5fdba9f7609306588e40d2
chronoclover <aquaprism_rhythm@outlook.com>
2020-03-11 01:42:02 +0900

    first commit for Forge 1.14.4

---
+++ src/main/java/noki/bouyomichat/ModInfo.java
@@ -0,0 +1,23 @@
+package noki.bouyomichat;
+
+
+/**********
+ * @class ModInfo
+ *
+ * @description このModのごく基本的な情報を格納します。
+ * @description_en Contains the mod's basic information.
+ */
+public class ModInfo {
+
+	//*****
+	//define member variables.
+	//*****	
+	public static final String ID = "bouyomichat";
+	public static final String NAME = "Bouyomi Chat";
+	public static final String VERSION = "2.8.0";
+	public static final String CHANNEL = ID;
+	public static final String DEPENDENCIES = "required-after:forge@[1.14.4-28.2.0,)";
+	public static final String MC_VERSIONS = "[1.14.4)";
+	//public static final String UPDATE_JSON = "https://dl.dropboxusercontent.com/s/c6v4e0kvir90gbe/bouyomichat2.json?dl=1";
+	
+}
